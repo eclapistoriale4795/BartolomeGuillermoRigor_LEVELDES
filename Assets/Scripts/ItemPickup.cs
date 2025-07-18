eddcbfaa266f44858cb1c461075199eebf177bf4
bhbbry <ryrybartolome@gmail.com>
2025-07-19 05:55:17 +0800

    bry

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
     public int objectID;
+
     [Header("Scriptable Object")]
     [SerializeField] private Item item;
     [SerializeField] private Player receiver;
@@ -15,14 +15,14 @@ public class ItemPickup : MonoBehaviour
         receiver.tempAmmo += item.incrementAmmo;
         if (receiver.tempAmmo > receiver.baseAmmo)
         {
-            int overloadedAmmo;
-            overloadedAmmo = receiver.tempAmmo - receiver.baseAmmo;
+            int overloadedAmmo = receiver.tempAmmo - receiver.baseAmmo;
             receiver.tempAmmo -= overloadedAmmo;
             receiver.ammo += overloadedAmmo;
         }
         item.canCollect = false;
-        Destroy(this.gameObject);
+        Destroy(gameObject);
     }
+
     void GetHealth()
     {
         if (receiver.tempHP < receiver.baseHP)
@@ -33,13 +33,14 @@ public class ItemPickup : MonoBehaviour
                 receiver.tempHP = receiver.baseHP;
             }
             item.canCollect = false;
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
         else
         {
-            print("You cannot get a health kit now, your health is full.");
+            Debug.Log("You cannot get a health kit now, your health is full.");
         }
     }
+
     void GetKey()
     {
         if (receiver.tempHP >= 101)
@@ -50,46 +51,46 @@ public class ItemPickup : MonoBehaviour
                 receiver.tempHP = receiver.baseHP;
             }
             item.canCollect = false;
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
         else
         {
-            print("You cannot get a health kit now, your health is full.");
+            Debug.Log("You cannot get a health kit now, your health is full.");
         }
     }
+
     private void Update()
     {
-        if (item.canCollect)
+        if (item.canCollect && Input.GetKeyDown(KeyCode.E))
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            switch (objectID)
             {
-                switch (objectID)
-                {
-                    case 0: GetAmmo(); break; //objectID = 0 -> Ammo Box Item
-                    case 1: GetHealth(); break; //objectID = 1 -> Health Kit Item
-                    case 2: GetKey(); break; //objectID = 2 -> Key for next level
-                }
+                case 0: GetAmmo(); break;
+                case 1: GetHealth(); break;
+                case 2: GetKey(); break;
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             item.canCollect = true;
         }
     }
+
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             item.canCollect = true;
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             item.canCollect = false;
         }
